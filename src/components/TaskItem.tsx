863ff05524b4abb6e47610ae13a16bddb0f24d1c
Alexander <mrreads@yandex.com>
2023-05-04 04:51:04 +0300

    priority

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,15 +1,18 @@
 import * as dayjs from 'dayjs'
 import 'dayjs/locale/ru'
 
-import ITask from "../types/ITask";
+import ITask, { EPriority } from "../types/ITask";
 
 export default function TaskItem({ data }: { data: ITask }) {
-    const { title, author_name, schedule } = data;
+    const { title, author_name, schedule, priority } = data;
     return (
         <div className="divide-y divide-gray-200 dark:divide-gray-700 flex flex-col py-4 px-3 mx-full max-w-full text-gray-900 bg-white rounded-lg shadow xl:p-3 lg:px-8 md:px-4 sm:px-2 dark:bg-gray-800 dark:text-white">
             <div>
-                <p className="pb-2 px-3 text-xs text-gray-500 dark:text-gray-400"> { dayjs(schedule.creation_time).locale('ru').format('D MMMM YYYY, hh:mm:ss') } </p>
-                <p className="pb-2 px-3 text-xl font-bold tracking-tight text-gray-900 dark:text-white 2xl:text-lg">{title}</p>
+                <div className='flex flex-row pb-2 px-3 items-center gap-2'>
+                    <div data-priority={priority} title={EPriority[priority]} /> 
+                    <p className=" text-xs text-gray-500 dark:text-gray-400 mt-[-1.5px]"> { dayjs(schedule.creation_time).locale('ru').format('D MMMM YYYY, hh:mm:ss') } </p>
+                </div>
+                <p className="pb-2 px-3 text-xl font-bold tracking-tight text-gray-900 dark:text-white 2xl:text-lg"> {title} </p>
             </div>
             <p className="pt-2 px-3 text-gray-500 dark:text-gray-400"> { author_name } </p>
         </div>
--- src/components/TasksWrapper.tsx
+++ src/components/TasksWrapper.tsx
@@ -18,7 +18,7 @@ export default function TasksWrapper({ tasks, status, text }: { tasks: ITask[], 
         <div className="drop-shadow flex flex-col flex-grow w-full max-w-sm gap-2 bg-gray-100 dark:bg-gray-700 p-4 pt-3 rounded-lg lg:max-w-none sm:px-2">
             <div className='px-1 lg:px-2'>
                 <p onClick={sortToggle} className="pt-1 text-gray-500 dark:text-gray-400 cursor-pointer md:text-sm">Сортировка: по { sortAsc ? 'возрастанию' : 'убыванию'} даты</p>
-                <h2 className="heading text-4xl mb-3 pt-2 uppercase">{text}</h2>
+                <h2 className="heading text-4xl mb-3 pt-2 uppercase"> {text}</h2>
             </div>
             { formatedTasks.map((task: ITask) => task.status == status ? <TaskItem data={task} key={task.id} /> : null) }
         </div>
