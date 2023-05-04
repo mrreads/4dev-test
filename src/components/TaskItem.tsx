e81cc9bf012dd47b91fb3a8cc502509ae7f8951c
Alexander <mrreads@yandex.com>
2023-05-04 04:29:07 +0300

    sorting

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,11 +1,17 @@
+import * as dayjs from 'dayjs'
+import 'dayjs/locale/ru'
+
 import ITask from "../types/ITask";
 
 export default function TaskItem({ data }: { data: ITask }) {
-    const { title, author_name } = data;
+    const { title, author_name, schedule } = data;
     return (
-        <div className="divide-y divide-gray-600 flex flex-col py-4 px-3 mx-full max-w-full text-gray-900 bg-white rounded-lg shadow xl:p-3 lg:px-8 md:px-4 sm:px-2 dark:bg-gray-800 dark:text-white">
-            <p className="pb-2 px-3 text-xl font-bold tracking-tight text-gray-900 dark:text-white 2xl:text-lg">{title}</p>
-            <p className="pt-1 px-3 text-gray-500 dark:text-gray-400"> { author_name } </p>
+        <div className="divide-y divide-gray-200 dark:divide-gray-700 flex flex-col py-4 px-3 mx-full max-w-full text-gray-900 bg-white rounded-lg shadow xl:p-3 lg:px-8 md:px-4 sm:px-2 dark:bg-gray-800 dark:text-white">
+            <div>
+                <p className="pb-2 px-3 text-xs text-gray-500 dark:text-gray-400"> { dayjs(schedule.creation_time).locale('ru').format('D MMMM YYYY, hh:mm:ss') } </p>
+                <p className="pb-2 px-3 text-xl font-bold tracking-tight text-gray-900 dark:text-white 2xl:text-lg">{title}</p>
+            </div>
+            <p className="pt-2 px-3 text-gray-500 dark:text-gray-400"> { author_name } </p>
         </div>
     )
 }
--- src/components/TasksWrapper.tsx
+++ src/components/TasksWrapper.tsx
@@ -1,3 +1,4 @@
+import * as dayjs from 'dayjs';
 import TaskItem from "./TaskItem";
 import { useState } from 'react';
 import ITask, { EStatus } from "../types/ITask";
@@ -5,12 +6,21 @@ import ITask, { EStatus } from "../types/ITask";
 export default function TasksWrapper({ tasks, status, text }: { tasks: ITask[], status: EStatus, text: string }) {
     const [sortAsc, setSortAsc] = useState(false);
     const sortToggle = () => setSortAsc(!sortAsc);
-
+    
+    const formatedTasks =
+    (sortAsc)
+    ?
+    tasks.sort((a: ITask, b: ITask) => dayjs(b.schedule.creation_time).unix() - dayjs(a.schedule.creation_time).unix())
+    :
+    tasks.sort((a: ITask, b: ITask) => dayjs(a.schedule.creation_time).unix() - dayjs(b.schedule.creation_time).unix());
+    
     return (
-        <div className="flex flex-col flex-grow w-full max-w-sm gap-2 bg-gray-50 dark:bg-gray-700 p-4 pt-3 rounded-lg lg:max-w-none sm:px-2">
-            <p onClick={sortToggle} className="pt-1 pt text-gray-500 dark:text-gray-400 cursor-pointer">Сортировка: по { sortAsc ? 'возрастанию' : 'убыванию'} даты</p>
-            <h2 className="heading text-4xl mb-3 pt-1">{text}</h2>
-            { tasks.map((task: ITask) => task.status == status ? <TaskItem data={task} /> : null) }
+        <div className="drop-shadow flex flex-col flex-grow w-full max-w-sm gap-2 bg-gray-100 dark:bg-gray-700 p-4 pt-3 rounded-lg lg:max-w-none sm:px-2">
+            <div className='px-1 lg:px-2'>
+                <p onClick={sortToggle} className="pt-1 text-gray-500 dark:text-gray-400 cursor-pointer md:text-sm">Сортировка: по { sortAsc ? 'возрастанию' : 'убыванию'} даты</p>
+                <h2 className="heading text-4xl mb-3 pt-2 uppercase">{text}</h2>
+            </div>
+            { formatedTasks.map((task: ITask) => task.status == status ? <TaskItem data={task} key={task.id} /> : null) }
         </div>
     )
 }
